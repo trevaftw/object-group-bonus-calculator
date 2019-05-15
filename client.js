5c88e84aaacaf00d8556b56ef0228ecb10c97fc2
Trevor Ramlow <trevorramlow@gmail.com>
2019-05-15 09:00:17 -0500

    completed bonus calculator

--- client.js
+++ client.js
@@ -40,4 +40,56 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+console.log('employees', employees);
+
+for (let i = 0; i < employees.length; i++) { //for each employee
+  bonusCalculater(employees[i]); //run the bonusCalculater function 
+}
+
+
+//check the review of each employee to determine their percent nicrease
+function bonusCalculater(employee) {
+  let bonusPercentage = 0
+  if (employee.reviewRating < 2) {
+    bonusPercentage = 0;
+  } else if (employee.reviewRating === 3) {
+    bonusPercentage = 0.04;
+  } else if (employee.reviewRating === 4) {
+    bonusPercentage = 0.06;
+  } else if (employee.reviewRating === 5) {
+    bonusPercentage = 0.10;
+  }
+
+  // check if they have a small employee number to get 5% bonus 
+  if (employee.employeeNumber.length === 4) { //if the employeeNumber is 4, log it. 
+    bonusPercentage += 0.05;
+  }
+
+  //check annual income
+  if (employee.annualSalary > 65000) {
+    bonusPercentage -= 0.01;
+  }
+
+  //check if percent greater 13 
+  if (bonusPercentage > 0.13) {
+    bonusPercentage = 0.13;
+  }
+
+  //chedck if percent less than 0
+  if (bonusPercentage < 0) {
+    bonusPercentage = 0;
+  }
+
+  
+
+  //create new object with info
+  let newObj = {};
+    newObj.name = employee.name;
+    newObj.bonusPercentage = bonusPercentage;
+    newObj.totalBonus = employee.annualSalary * bonusPercentage;
+    newObj.totalCompensation = Number(newObj.totalBonus) + Number(employee.annualSalary);
+  
+  console.log(newObj);
+
+}
+
